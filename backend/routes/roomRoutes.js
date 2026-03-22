cbab61b846b729cf6b3f4673558f0829fe5f40c2
joshnajohn0202-art <joshnajohn0202@gmail.com>
2026-03-22 08:58:02 +0530

    Delete backend directory

--- backend/routes/roomRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getRooms, addRoom } = require("../controllers/roomController");
-
-router.get("/", getRooms);
-router.post("/", addRoom);
-
-module.exports = router;
